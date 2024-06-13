aaa9945a6703340c1a884ef1ee195fbcb3587bc9
ankita <sharma117ankita@gmail.com>
2024-06-13 09:35:42 +0530

    implemented design for home screen and added add edit file

---
+++ lib/screens/AddEdit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddEdit extends StatefulWidget {
+  const AddEdit({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+   return _AddEditState();
+  }
+  
+}
+
+class _AddEditState extends State<AddEdit> {
+  @override
+  Widget build(BuildContext context) {
+   return
+  }
+
+}
--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Color.fromRGBO(233, 99, 108, 25),
+            margin: EdgeInsets.all(10), // Adds margin around the card
+            elevation: 4, // Elevation for the shadow effect
+            child: ListTile(
+              leading: Icon(Icons.star,color: Colors.yellow,),
+              title: Text('Rating',style: TextStyle(color: Colors.white,),),
+              subtitle: Text('Movie Name',style: TextStyle(color: Colors.white,)),
+              onTap: () {
+                // Add your onTap logic here if needed
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
-
 }
